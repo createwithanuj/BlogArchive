3736e05f157d5eccb7b9379242ec224ee65a720b
Anuj Kumar Gautam <61941950+createwithanuj@users.noreply.github.com>
2025-04-03 04:17:47 +0000

    Add middleware for logging and error handling; integrate CORS support

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const cors = require('cors');
 
 const app = express();
 
@@ -17,7 +19,12 @@ mongoose.connect(config.mongoURL)
 });
 
 app.use(express.json());
+app.use(middleware.requestLogger);
+
 
 app.use('/api/blogs', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
